de893300de5e89abbaaef45ce6d2a7a779efd0c2
hanna <hanna0115@naver.com>
2025-03-16 20:39:28 +0900

    [feat] 메인페이지 마크업 완료 #S12P21B107-40

--- Frontend/src/components/home/CharacterChange.tsx
+++ Frontend/src/components/home/CharacterChange.tsx
@@ -1,10 +1,14 @@
 import styles from './CharacterChange.module.css'
+import opigi from '../../assets/images/opigi.png'
 
 type Props = {}
 
 function CharacterChange({}: Props) {
   return (
-    <div>CharacterChange</div>
+    <div className={styles['character-change']}>
+      <p>[학습의지가 불타서 행복해!]</p>
+      <img src={opigi} alt="" />
+    </div>
   )
 }
 
--- Frontend/src/components/home/LevelChart.tsx
+++ Frontend/src/components/home/LevelChart.tsx
@@ -1,11 +1,20 @@
-import styles from './LevelChart.module.css'
+import styles from "./LevelChart.module.css";
 
-type Props = {}
+type Props = {};
 
 function LevelChart({}: Props) {
   return (
-    <div>LevelChart</div>
-  )
+    <div className={styles["level-chart"]}>
+      <h2>실력 향상 추이</h2>
+      {/* 차트 자리 */}
+
+      {/* 커스텀 범례 */}
+      <div className={styles["chart-legend"]}>
+        <div className={styles["legend-dot"]}></div>
+        <p className={styles["legend-text"]}>모의고사 점수</p>
+      </div>
+    </div>
+  );
 }
 
-export default LevelChart
+export default LevelChart;
