62769893c4a239d19f73d8a689245ca6438aaae1
wngml00118 <wngml00118@naver.com>
2025-03-22 04:30:15 +0900

    [error] 충돌 해결: AuthRoutes.tsx 삭제 유지

--- Frontend/src/pages/auth/Profile.tsx
+++ Frontend/src/pages/auth/Profile.tsx
@@ -11,9 +11,18 @@ function ProfilePage({}: Props) {
   };
 
   const handleNext = () => {
-    // 다음 페이지로 이동하는 로직
-    console.log("다음 버튼 클릭");
+    if(!formData.wishGrade || !formData.currentGrade || !formData.examDate || !formData.agreeToTerms) {
+      alert("모든 항목을 입력해주세요.");
+      console.log("다음 버튼 클릭");
+      return;
+    }
   };
+  navigate("/auth/survey", {state: {profileData: formData}});
+
+  localStorage.setItem("profileData", JSON.stringify(formData));
+  navigate("/auth/survey");
+  console.log("다음 버튼 클릭");
+
 
   return (
     <div className={styles["profile-container"]}>
@@ -27,7 +36,9 @@ function ProfilePage({}: Props) {
         <select 
           name="wishGrade" 
           className={styles.select}
-          defaultValue=""
+          value={formData.wishGrade}
+          onChange={handleInputChange}
+          style={{ color: formData.wishGrade ? '#000000' : '#A3A3A3' }}
         >
           <option value="" disabled>희망하는 등급을 선택하세요.</option>
           <option value="AL">AL</option>
@@ -47,7 +58,9 @@ function ProfilePage({}: Props) {
         <select 
           name="currentGrade" 
           className={styles.select}
-          defaultValue=""
+          value={formData.currentGrade}
+          onChange={handleInputChange}
+          style={{ color: formData.currentGrade ? '#000000' : '#A3A3A3' }}
         >
           <option value="" disabled>현재 등급을 선택하세요.</option>
           <option value="AL">AL</option>
@@ -69,6 +82,8 @@ function ProfilePage({}: Props) {
           type="date" 
           name="examDate" 
           className={styles["date-input"]}
+          value={formData.examDate}
+          onChange={handleInputChange}
         />
         </div>
       </div>
@@ -82,7 +97,7 @@ function ProfilePage({}: Props) {
           <div className={styles["terms-content"]}>
             <p>개인정보 수집 및 이용 동의 (필수)</p>
             <p>(주)오마이픽은 서비스 제공을 위해 아래와 같이 개인정보를 수집 및 이용합니다. 동의를 거부할 권리가 있으며, 동의 거부 시 서비스 이용이 제한됩니다.</p>
-            <p className={styles["terms-detail"]}>* 수집 목적: 서비스 제공 및 계정 관리</p>
+            <p className={styles["terms-detail"]}>* 수집 목적 : 서비스 제공 및 계정 관리</p>
           </div>
           <div className={styles["checkbox-container"]}>
             <input 
@@ -90,6 +105,8 @@ function ProfilePage({}: Props) {
               id="agreeToTerms" 
               name="agreeToTerms" 
               className={styles.checkbox}
+              checked={formData.agreeToTerms}
+              onChange={handleInputChange}
             />
             <label htmlFor="agreeToTerms" className={styles["checkbox-label"]}>
               위 개인정보 수집 및 이용에 동의합니다.
