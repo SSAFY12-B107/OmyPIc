fe3a117dfa9dbb5a2bef6fedbc7c26a844db1d89
hyejoony <jeonbok.joha@gmail.com>
2025-03-21 17:27:14 +0900

    [feat] 피드백 데이터 탠스택쿼리 hook 설정

--- Frontend/src/hooks/useHistory.ts
+++ Frontend/src/hooks/useHistory.ts
@@ -2,46 +2,56 @@
 import { useQuery } from '@tanstack/react-query';
 import apiClient from '../api/apiClient';
 
-// 타입 정의
+
+export interface Scores {
+  total_score : string | null ;
+  comboset_score: string | null;
+  roleplaying_score: string | null;
+  unexpected_score: string | null;
+}
+
 export interface TestHistory {
   id: string;
   test_date: string;
   test_type: boolean;
-  test_score: string | null;
+  test_score: Scores | null;
 }
 
 export interface AverageScore {
-  total_score: string;
-  comboset_score: string;
-  roleplaying_score: string;
-  unexpected_score: string;
+  total_score: string | null;
+  comboset_score: string | null;
+  roleplaying_score: string | null;
+  unexpected_score: string | null;
 }
 
 export interface UserHistoryResponse {
-  average_score: AverageScore;
+  average_score: AverageScore | null;
   test_history: TestHistory[];
 }
 
 
+
 // 사용자 히스토리 조회 함수
-const fetchUserHistory = async (user_pk: number): Promise<UserHistoryResponse> => {
+const fetchUserHistory = async (user_pk: string): Promise<UserHistoryResponse> => {
     try {
-      const { data } = await apiClient.get(`/api/tests/history/${user_pk}`);
+      const { data } = await apiClient.get(`/tests/history/${user_pk}`);
       return data;
+
+  
     } catch (error) {
+
       console.error('히스토리 조회 에러:', error);
+      
       throw error;
     }
   };
   
   // 사용자 히스토리 조회 훅
   export const useUserHistory = (user_pk: string | undefined) => {
-      // 문자열을 숫자로 변환
-  const numericUserId = user_pk ? parseInt(user_pk, 10) : undefined;
 
     return useQuery({
       queryKey: ['userHistory', user_pk],
-      queryFn: () => fetchUserHistory(numericUserId!),
+      queryFn: () => fetchUserHistory(user_pk!),
       // userId가 없을 경우 쿼리 실행 중지
       enabled: !!user_pk,
 
