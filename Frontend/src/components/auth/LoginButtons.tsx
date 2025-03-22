af9450ffbc7d82f8653fbdc33275d6822008b11e
hanna <hanna0115@naver.com>
2025-03-22 16:18:44 +0900

    [fix] 로그인 버튼 깨진 스타일 수정

--- Frontend/src/components/auth/LoginButtons.tsx
+++ Frontend/src/components/auth/LoginButtons.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import styles from './LoginButtons.module.css'
 import naverLogin from '../../assets/images/naver_login.png'
 import googleLogin from '../../assets/images/google_login.png'
