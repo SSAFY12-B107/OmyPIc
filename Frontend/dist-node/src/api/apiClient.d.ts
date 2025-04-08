813a8d477da07cf9d5fc871b9da77defa2a4f5a4
JuHee Kim <wngml00118@naver.com>
2025-04-08 14:25:13 +0900

    [refactor/juhee]이미지 파일 압축

--- Frontend/dist-node/src/api/apiClient.d.ts
+++ Frontend/dist-node/src/api/apiClient.d.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from 'axios';
 type NavigateFunction = () => void;
 export declare const setNavigateFunction: (customNavigate: NavigateFunction) => void;
+export declare const setNavigateTo404Function: (customNavigate: NavigateFunction) => void;
 declare const apiClient: AxiosInstance;
 export default apiClient;
--- Frontend/dist-node/src/store/testSlice.d.ts
+++ Frontend/dist-node/src/store/testSlice.d.ts
@@ -1,4 +1,14 @@
 import { PayloadAction } from "@reduxjs/toolkit";
+export interface SingleProblem {
+    test_id: string;
+    problem_id: string;
+    problem_category: string;
+    topic_category: string;
+    content: string;
+    audio_s3_url: string;
+    high_grade_kit: boolean;
+    user_id: string;
+}
 export interface Test {
     _id: string;
     audio_s3_url?: string;
@@ -13,14 +23,18 @@ export interface Test {
             topic_category: string;
             problem: string;
             audio_s3_url: string;
+            processing_status?: string;
+            processing_message?: string;
         };
     };
 }
 export interface TestState {
     currentTest: Test | null;
+    currentSingleProblem: SingleProblem | null;
+    isRandomProblem: boolean;
 }
 export declare const testActions: import("@reduxjs/toolkit").CaseReducerActions<{
-    setCurrentTest: (state: import("immer").WritableDraft<TestState>, action: PayloadAction<Test>) => void;
+    setCurrentTest: (state: import("immer").WritableDraft<TestState>, action: PayloadAction<Test | SingleProblem>) => void;
 }, "tests">;
 declare const _default: import("redux").Reducer<TestState>;
 export default _default;
